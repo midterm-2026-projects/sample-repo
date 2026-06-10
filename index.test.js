087e4af9c33d7075c4ac4f2c8dd74b6f86cc32c8
gabify <ivangabriel.talaoc@g.batstate-u.edu.ph>
2026-06-10 10:13:16 +0800

    Add login feature

--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
 import {expect, test} from "vitest";
-import {add, divide} from "./index.js";
+import {add, divide, login} from "./index.js";
 
 test("Should add numbers in an array", ()=>{
     const numbers = [1,2,3,4,5];
@@ -14,4 +14,16 @@ test("Should divide two numbers", () =>{
     const result = divide(a, b);
 
     expect(result).toBe("Cannot divide by zero");
+})
+
+test("Should return true with correct credentials", ()=>{
+    const result = login("admin", "password");
+
+    expect(result).toBe(true);
+})
+
+test("Should return false with incorrect credentials", ()=>{
+    const result = login("not admin", "password");
+
+    expect(result).toBe(false);
 })
