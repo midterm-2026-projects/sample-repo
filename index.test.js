a51fe87b2f31134aab1f7679329c36307f72f4b7
gabify <ivangabriel.talaoc@g.batstate-u.edu.ph>
2026-06-10 09:20:54 +0800

    Add new feature

--- index.test.js
+++ index.test.js
@@ -1,9 +1,17 @@
 import {expect, test} from "vitest";
-import {add} from "./index.js";
+import {add, divide} from "./index.js";
 
 test("Should add numbers in an array", ()=>{
     const numbers = [1,2,3,4,5];
     const result = add(numbers);
 
     expect(result).toBe(15);
+})
+
+test("Should divide two numbers", () =>{
+    const a = 10;
+    const b = 0;
+    const result = divide(a, b);
+
+    expect(result).toBe(0);
 })
